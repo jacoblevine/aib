de238aeab0d54ab8634f678486a592d01d7b28f1
Jacob Levine <jl3545@columbia.edu>
2015-07-16 18:31:06 -0400

    Add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AIB',
+    version='0.1',
+    packages=[''],
+    url='https://www.github.com/jacoblevine/aib',
+    license='Apache License 2.0',
+    author='Jacob Levine',
+    author_email='jl3545@columbia.edu',
+    description='Agglomerative Information Bottleneck algorithm'
+)
